4a3948e82e6f0f05d7bde8917acd4dcd778bef1e
Lewis Parmenter <lewis.o.parmenter@gmail.com>
2023-06-02 12:08:48 +1000

    Added defaults saving.

--- WireGuardCommand/MainWindow.xaml.cs
+++ WireGuardCommand/MainWindow.xaml.cs
@@ -23,6 +23,48 @@ namespace WireGuardCommand
         public MainWindow()
         {
             InitializeComponent();
+
+            LoadDefaults();
+        }
+
+        private void LoadDefaults()
+        {
+            InputPrefix.Text = Properties.Settings.Default.DefaultPrefix;
+            InputCommand.Text = Properties.Settings.Default.DefaultCommand;
+            InputPostfix.Text = Properties.Settings.Default.DefaultPostfix;
+
+            InputListenPort.Text = Properties.Settings.Default.DefaultListenPort;
+            InputNoClients.Text = Properties.Settings.Default.DefaultNoClients;
+            InputSubnet.Text = Properties.Settings.Default.DefaultSubnet;
+
+            InputIPs.Text = Properties.Settings.Default.DefaultIPs;
+            InputEndpoint.Text = Properties.Settings.Default.DefaultEndpoint;
+            InputDNS.Text = Properties.Settings.Default.DefaultDNS;
+
+            InputInterface.Text = Properties.Settings.Default.DefaultInterface;
+
+            CheckBoxPresharedKeys.IsChecked = Properties.Settings.Default.DefaultPresharedKeys;
+        }
+
+        private void ButtonSaveDefaults_Click(object sender, RoutedEventArgs e)
+        {
+            Properties.Settings.Default.DefaultPrefix = InputPrefix.Text;
+            Properties.Settings.Default.DefaultCommand = InputCommand.Text;
+            Properties.Settings.Default.DefaultPostfix = InputPostfix.Text;
+
+            Properties.Settings.Default.DefaultListenPort = InputListenPort.Text;
+            Properties.Settings.Default.DefaultNoClients = InputNoClients.Text;
+            Properties.Settings.Default.DefaultSubnet = InputSubnet.Text;
+
+            Properties.Settings.Default.DefaultIPs = InputIPs.Text;
+            Properties.Settings.Default.DefaultEndpoint = InputEndpoint.Text;
+            Properties.Settings.Default.DefaultDNS = InputDNS.Text;
+
+            Properties.Settings.Default.DefaultInterface = InputInterface.Text;
+
+            Properties.Settings.Default.DefaultPresharedKeys = CheckBoxPresharedKeys.IsChecked.HasValue ? CheckBoxPresharedKeys.IsChecked.Value : false;
+
+            Properties.Settings.Default.Save();
         }
     }
 }
