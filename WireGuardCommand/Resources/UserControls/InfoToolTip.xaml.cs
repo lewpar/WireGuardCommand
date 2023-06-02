77f036eaf759b56b1af30f90dadb5d49ac4f61ec
Lewis Parmenter <lewis.o.parmenter@gmail.com>
2023-06-02 10:28:40 +1000

    Added InfoTooltip UserControl.

---
+++ WireGuardCommand/Resources/UserControls/InfoToolTip.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WireGuardCommand.Resources.UserControls
+{
+    /// <summary>
+    /// Interaction logic for InfoTooltip.xaml
+    /// </summary>
+    public partial class InfoToolTip : UserControl
+    {
+        public string Information
+        {
+            get { return (string)GetValue(InformationProperty); }
+            set { SetValue(InformationProperty, value); }
+        }
+
+        public static readonly DependencyProperty InformationProperty =
+            DependencyProperty.Register("Information", typeof(string), typeof(InfoToolTip), new PropertyMetadata(default(string)));
+
+
+        public string Header
+        {
+            get { return (string)GetValue(HeaderProperty); }
+            set { SetValue(HeaderProperty, value); }
+        }
+
+        public static readonly DependencyProperty HeaderProperty =
+            DependencyProperty.Register("Header", typeof(string), typeof(InfoToolTip), new PropertyMetadata(default(string)));
+
+
+        public InfoToolTip()
+        {
+            InitializeComponent();
+        }
+    }
+}
