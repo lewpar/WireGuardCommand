376be95e006636dcece01c5258278679f65030ef
Lewis Parmenter <lewis.o.parmenter@gmail.com>
2023-06-09 09:56:44 +1000

    Added open folder dialog for saving files.

--- WireGuardCommand/MainWindow.xaml.cs
+++ WireGuardCommand/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -70,6 +71,39 @@ namespace WireGuardCommand
 
         private void ButtonGenerate_Click(object sender, RoutedEventArgs e)
         {
+            string saveLocation = string.Empty;
+
+            if(!string.IsNullOrEmpty(Properties.Settings.Default.SaveLocation))
+            {
+                saveLocation = Properties.Settings.Default.SaveLocation;
+            }
+
+            using (var saveFileDialog = new System.Windows.Forms.FolderBrowserDialog())
+            {
+                var saveResult = saveFileDialog.ShowDialog();
+                if (saveResult == System.Windows.Forms.DialogResult.OK)
+                {
+                    Properties.Settings.Default.SaveLocation = saveFileDialog.InitialDirectory;
+                    Properties.Settings.Default.Save();
+
+                    if(string.IsNullOrEmpty(saveFileDialog.SelectedPath))
+                    {
+                        MessageBox.Show("Save path is empty.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                        return;
+                    }
+
+                    try
+                    {
+                        File.WriteAllText(saveFileDialog.SelectedPath, "");
+                        File.WriteAllText(saveFileDialog.SelectedPath, "");
+                    }
+                    catch(Exception ex)
+                    {
+                        MessageBox.Show($"Failed to save files with exception: {ex.GetType()}\n{ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                        return;
+                    }
+                }
+            }
         }
 
         private void ButtonClearDefaults_Click(object sender, RoutedEventArgs e)
--- WireGuardCommand/Properties/Settings.Designer.cs
+++ WireGuardCommand/Properties/Settings.Designer.cs
@@ -155,5 +155,17 @@ namespace WireGuardCommand.Properties {
                 this["DefaultSubnet"] = value;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string SaveLocation {
+            get {
+                return ((string)(this["SaveLocation"]));
+            }
+            set {
+                this["SaveLocation"] = value;
+            }
+        }
     }
 }
