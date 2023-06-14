b240f202d51268ee021f23a1890f02dc48b6c5e4
Lewis Parmenter <lewis.o.parmenter@gmail.com>
2023-06-14 15:14:13 +1000

    Fixed comparison of byte arrays.

--- WireGuardCommand/ToolsWindow.xaml.cs
+++ WireGuardCommand/ToolsWindow.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using Elliptic;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace WireGuardCommand
 {
@@ -46,7 +36,7 @@ namespace WireGuardCommand
 
                 byte[] curvePublicKey = Curve25519.GetPublicKey(privateKey);
 
-                bool result = curvePublicKey == publicKey;
+                bool result = curvePublicKey.SequenceEqual(publicKey);
 
                 if(result)
                 {
