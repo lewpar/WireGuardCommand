67e9d6f11c4d0e4d3e75e9da3587592d278ea765
lewpar <lewis.o.parmenter@gmail.com>
2023-12-21 11:48:48 +1100

    Binded and set defaults for the rest of config entries.

--- WireGuardCommand/Models/Project/WGCConfig.cs
+++ WireGuardCommand/Models/Project/WGCConfig.cs
@@ -18,11 +18,31 @@ namespace WireGuardCommand.Models.Project
         [ObservableProperty]
         private string? seed;
 
+        [ObservableProperty]
+        private string? allowedIPs;
+
+        [ObservableProperty]
+        private string? endpoint;
+
+        [ObservableProperty]
+        private string? dns;
+
+        [ObservableProperty]
+        private string? postUpRule;
+
+        [ObservableProperty]
+        private string? postDownRule;
+
         public WGCConfig()
         {
             ListenPort = 51820;
             NoOfClients = 1;
             Cidr = "10.0.0.0/24";
+            AllowedIPs = "0.0.0.0/0, ::/0";
+            Endpoint = string.Empty;
+            Dns = string.Empty;
+            PostUpRule = "iptables -A FORWARD -i %i -j ACCEPT; iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE";
+            PostDownRule = "iptables -D FORWARD -i %i -j ACCEPT; iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE";
 
             Seed = RandomHelper.GetRandomSeed();
         }
