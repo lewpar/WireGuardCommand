11d41e8b62c80cd8f6d16fcd8d70993b35af34f9
lewpar <lewis.o.parmenter@gmail.com>
2023-12-29 09:57:08 +1100

    Added input validation for numbers and cidr notation.

---
+++ WireGuardCommand/Validation/CidrRule.cs
@@ -0,0 +1,43 @@
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+using System.Windows.Controls;
+
+namespace WireGuardCommand.Validation
+{
+    public class CidrRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            string? input = value as string;
+
+            if (input is null)
+            {
+                return new ValidationResult(false, "There was an error while validating input.");
+            }
+
+            if (value is not string)
+            {
+                return new ValidationResult(false, "There was an error while validating input.");
+            }
+
+            string[] parts = input.Split('/');
+
+            if(parts.Length < 2)
+            {
+                return new ValidationResult(false, "Invalid cidr notation.");
+            }
+
+            if (!Regex.Match(parts[0], "[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}").Success)
+            {
+                return new ValidationResult(false, "Invalid address part.");
+            }
+
+            if (!int.TryParse(parts[1], out int cidr))
+            {
+                return new ValidationResult(false, "Invalid cidr part.");
+            }
+
+            return ValidationResult.ValidResult;
+        }
+    }
+}
---
+++ WireGuardCommand/Validation/NumberRule.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+using System.Windows.Controls;
+
+namespace WireGuardCommand.Validation
+{
+    public class NumberRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if(value is null)
+            {
+                return new ValidationResult(false, "There was an error validating input.");
+            }
+
+            if(!int.TryParse(value as string, out int result))
+            {
+                return new ValidationResult(false, "You must input a number.");
+            }
+
+            return ValidationResult.ValidResult;
+        }
+    }
+}
