b185b810301a3a08b7feda2e8f204fca5825577f
lewpar <lewis.o.parmenter@gmail.com>
2024-09-23 09:49:43 +1000

    Added error banner to project view.

--- WireGuardCommand/Pages/Project/ProjectView.razor.cs
+++ WireGuardCommand/Pages/Project/ProjectView.razor.cs
@@ -1,7 +1,11 @@
 ﻿using Microsoft.AspNetCore.Components;
+
 using System.Security.Cryptography;
+using System.Text.Json;
+
 using WireGuardCommand.Extensions;
 using WireGuardCommand.Services;
+using WireGuardCommand.Services.Models;
 
 namespace WireGuardCommand.Pages.Project;
 
@@ -16,6 +20,20 @@ public partial class ProjectView
     [Inject]
     public NavigationManager NavigationManager { get; set; } = default!;
 
+    public string? Error { get; set; }
+
+    public bool HasUnsavedChanges
+    {
+        get => HasChanges();
+    }
+
+    private ProjectData? originalData;
+
+    protected override void OnAfterRender(bool firstRender)
+    {
+        originalData = Cache.CurrentProject.ProjectData;
+    }
+
     private void GoBack()
     {
         Cache.Clear();
@@ -27,9 +45,25 @@ public partial class ProjectView
         var project = Cache.CurrentProject;
         if(project.ProjectData is null)
         {
+            Error = "Failed to generate seed.";
             return;
         }
 
         project.ProjectData.Seed = RandomNumberGenerator.GetBytes(32).ToBase64();
+
+        StateHasChanged();
+    }
+
+    private bool HasChanges()
+    {
+        if(originalData is null || 
+            Cache.CurrentProject.ProjectData is null)
+        {
+            Error = "Unable to determine changes.";
+            return false;
+        }
+
+        return JsonSerializer.Serialize(originalData) !=
+            JsonSerializer.Serialize(Cache.CurrentProject.ProjectData);
     }
 }
